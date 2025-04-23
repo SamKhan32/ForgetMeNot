6b17d3d60ad561d41f28f9df9da7c1fdff9fd04f
devantsyp <antsypovichvladislav@gmail.com>
2025-04-23 19:31:18 -0400

    created the Assignment model

--- main/base/models.py
+++ main/base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Assignment(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    course_name = models.CharField(max_length=200)
+    due_date = models.DateTimeField()
