1e8587cec59bcce5a8705e637810a485d702f284
Sam Khan <psam1625@gmail.com>
2025-04-28 17:54:02 -0400

    updated customUserCreationForm to include proper date and timezone fields

--- main/base/forms.py
+++ main/base/forms.py
@@ -2,8 +2,25 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
-
+# Define your timezone choices
+TIMEZONE_CHOICES = [
+    ('EST', 'Eastern Standard Time'),
+    ('CST', 'Central Standard Time'),
+    ('MST', 'Mountain Standard Time'),
+    ('PST', 'Pacific Standard Time'),
+    # you can add more if you want
+]
 class CustomUserCreationForm(UserCreationForm):
+    
+    birthdate = forms.DateField(
+    required=False,
+    widget=forms.DateInput(attrs={'type': 'date'})
+    )
+    timezone = forms.ChoiceField(
+    choices=TIMEZONE_CHOICES,
+    required=True
+    )
     class Meta:
         model = CustomUser
+
         fields = ['username', 'email', 'birthdate', 'timezone', 'password1', 'password2']
